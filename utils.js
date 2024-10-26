7ead32d654ac86d64d822cddd6b69edf71a26569
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-10-26 18:36:08 +0200

    add score functionality to utils

--- utils.js
+++ utils.js
@@ -1,27 +1,25 @@
+let username = "";
+let score = 0;
 
-let username;
-let score =0;
-
-
-function saveUserName(name){
-    if(typeof name !== "string"){
-        throw new Error("Enter valid name")
-    }
-    if(!name){
-        throw new Error("Name is required")
-    }
-    username=name
+function saveUserName(name) {
+  if (typeof name !== "string") {
+    throw new Error("Enter valid name");
+  }
+  if (!name.trim()) {
+    throw new Error("Name is required");
+  }
+  username = name.trim();
 }
 
+function increaseScore(isCorrect) {
+  if (isCorrect) {
+    score += 1;
+  }
+  return score;
+}
 
+function clearScore (){
+    score = 0
+}
 
-
- saveUserName("sisanda")
-
-console.log(username)
-
-
-
-
-
-export {username, saveUserName}
+export { username, saveUserName, increaseScore, score , clearScore};
