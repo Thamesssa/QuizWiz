fbb0f4fa8156862496e49d1b0e1030cf290009d5
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-10-26 18:39:03 +0200

    implement  functions from utils to the dom

--- script.js
+++ script.js
@@ -1,70 +1,78 @@
-import { username, saveUserName } from "./utils.js";
+import { username, saveUserName, increaseScore } from "./utils.js";
 import { quizQuestions } from "./quizQuestions.js";
 
-const quiz = document.getElementById('quiz');
-const answerEls = document.querySelectorAll('.answer');
-const questionEl = document.getElementById('question');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
-const submitBtn = document.getElementById('submit');
+// Get elements from HTML
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const nextQuestionBtn = document.getElementById("submit");
+const startQuizBtn = document.getElementById("start-quiz");
 
-// Shuffle the quizQuestions array
+// Shuffle and limit the quizQuestions array
 const shuffledQuestions = quizQuestions.sort(() => 0.5 - Math.random());
-
-// Limit the number of questions to 5
 const limitedQuestions = shuffledQuestions.slice(0, 5);
 
 let currentQuiz = 0;
 let score = 0;
 
-loadQuiz();
-
 function loadQuiz() {
-    deselectAnswers();
+  deselectAnswers();
 
-    const currentQuizData = limitedQuestions[currentQuiz];
-
-    questionEl.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.options[0];
-    b_text.innerText = currentQuizData.options[1];
-    c_text.innerText = currentQuizData.options[2];
-    d_text.innerText = currentQuizData.options[3];
+  const currentQuizData = limitedQuestions[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.options[0];
+  b_text.innerText = currentQuizData.options[1];
+  c_text.innerText = currentQuizData.options[2];
+  d_text.innerText = currentQuizData.options[3];
 }
 
 function deselectAnswers() {
-    answerEls.forEach(answerEl => answerEl.checked = false);
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
 }
 
 function getSelected() {
-    let answer;
-
-    answerEls.forEach(answerEl => {
-        if (answerEl.checked) {
-            answer = answerEl.id;
-        }
-    });
-
-    return answer;
+  let answer;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
 }
 
-submitBtn.addEventListener('click', () => {
-    const answer = getSelected();
+startQuizBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  saveUserName(name);
+  localStorage.setItem("username", name);
+  document.getElementById("start-page").style.display = "none";
+  quiz.style.display = "flex";
+  document.getElementById(
+    "welcome-message"
+  ).textContent = `Good luck, ${name}!`;
+  loadQuiz();
+});
 
-    if (answer) {
-        if (answer === limitedQuestions[currentQuiz].answer) {
-            score++;
-        }
-        currentQuiz++;
+nextQuestionBtn.addEventListener("click", () => {
+  const answer = getSelected();
 
-        if (currentQuiz < limitedQuestions.length) {
-            loadQuiz();
-        } else {
-            quiz.innerHTML = `
-                <h2>You answered ${score}/${limitedQuestions.length} questions correctly</h2>
-                <button onclick="location.reload()">Reload</button>
+  if (answer) {
+    const isCorrect = answer === limitedQuestions[currentQuiz].answer;
+    score = increaseScore(isCorrect);
+
+    currentQuiz++;
+
+    if (currentQuiz < limitedQuestions.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+                 <h2>You answered ${score}/${limitedQuestions.length} questions correctly</h2>
+                <button onclick="location.reload()">Restart Quiz</button>
             `;
-        }
     }
+  }
 });
